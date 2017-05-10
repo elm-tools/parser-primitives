977acefa8c2d01365918540024edcdf2cdbdf94f
Evan Czaplicki <evancz@users.noreply.github.com>
2017-05-10 14:55:59 -0700

    Mostly convert to new JS format

--- src/Elm/Kernel/ParserPrimitives.js
+++ src/Elm/Kernel/ParserPrimitives.js
@@ -1,16 +1,15 @@
-var _elm_tools$parser_primitives$Native_ParserPrimitives = function() {
 
 
 // STRINGS
 
-function isSubString(smallString, offset, row, col, bigString)
+var _ParserPrimitives_isSubString = F5(function(smallString, offset, row, col, bigString)
 {
 	var smallLength = smallString.length;
 	var bigLength = bigString.length - offset;
 
 	if (bigLength < smallLength)
 	{
-		return tuple3(-1, row, col);
+		return _ParserPrimitives_tuple3(-1, row, col);
 	}
 
 	for (var i = 0; i < smallLength; i++)
@@ -19,7 +18,7 @@ function isSubString(smallString, offset, row, col, bigString)
 
 		if (char !== bigString[offset + i])
 		{
-			return tuple3(-1, row, col);
+			return _ParserPrimitives_tuple3(-1, row, col);
 		}
 
 		// if it is a two word character
@@ -28,7 +27,7 @@ function isSubString(smallString, offset, row, col, bigString)
 			i++
 			if (smallString[i] !== bigString[offset + i])
 			{
-				return tuple3(-1, row, col);
+				return _ParserPrimitives_tuple3(-1, row, col);
 			}
 			col++;
 			continue;
@@ -46,10 +45,10 @@ function isSubString(smallString, offset, row, col, bigString)
 		col++
 	}
 
-	return tuple3(offset + smallLength, row, col);
-}
+	return _ParserPrimitives_tuple3(offset + smallLength, row, col);
+});
 
-function tuple3(a, b, c)
+function _ParserPrimitives_tuple3(a, b, c)
 {
 	return { ctor: '_Tuple3', _0: a, _1: b, _2: c };
 }
@@ -57,9 +56,7 @@ function tuple3(a, b, c)
 
 // CHARS
 
-var mkChar = _elm_lang$core$Native_Utils.chr;
-
-function isSubChar(predicate, offset, string)
+var _ParserPrimitives_isSubChar = F3(function(predicate, offset, string)
 {
 	if (offset >= string.length)
 	{
@@ -68,28 +65,28 @@ function isSubChar(predicate, offset, string)
 
 	if ((string.charCodeAt(offset) & 0xF800) === 0xD800)
 	{
-		return predicate(mkChar(string.substr(offset, 2)))
+		return predicate(_elm_lang$core$Native_Utils.chr(string.substr(offset, 2)))
 			? offset + 2
 			: -1;
 	}
 
 	var char = string[offset];
 
-	return predicate(mkChar(char))
+	return predicate(_elm_lang$core$Native_Utils.chr(char))
 		? ((char === '\n') ? -2 : (offset + 1))
 		: -1;
-}
+});
 
 
 // FIND STRING
 
-function findSubString(before, smallString, offset, row, col, bigString)
+var _ParserPrimitives_findSubString = F6(function(before, smallString, offset, row, col, bigString)
 {
 	var newOffset = bigString.indexOf(smallString, offset);
 
 	if (newOffset === -1)
 	{
-		return tuple3(-1, row, col);
+		return _ParserPrimitives_tuple3(-1, row, col);
 	}
 
 	var scanTarget = before ? newOffset	: newOffset + smallString.length;
@@ -117,14 +114,5 @@ function findSubString(before, smallString, offset, row, col, bigString)
 		col++;
 	}
 
-	return tuple3(offset, row, col);
-}
-
-
-return {
-	isSubString: F5(isSubString),
-	isSubChar: F3(isSubChar),
-	findSubString: F6(findSubString)
-};
-
-}();
+	return _ParserPrimitives_tuple3(offset, row, col);
+});
